d2f2e9146aa56301f4c3aa408fd64f04941e0c89
Jesper Wistrom <theace4k@gmail.com>
2023-11-03 20:19:47 +0100

    Handle result when using commands

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,19 @@ fn main() -> Result<()> {
     match cli.command {
         args::Commands::Encode(args) => {
             println!("encode {:#?}", args);
-            commands::encode(args);
+            commands::encode(args)?;
         }
         args::Commands::Decode(args) => {
             println!("decode {:?}", args);
-            commands::decode(args);
+            commands::decode(args)?;
         }
         args::Commands::Remove(args) => {
             println!("remove {:?}", args);
-            commands::remove(args);
+            commands::remove(args)?;
         }
         args::Commands::Print(args) => {
             println!("print {:?}", args);
-            commands::print(args);
+            commands::print(args)?;
         }
     }
     Ok(())
