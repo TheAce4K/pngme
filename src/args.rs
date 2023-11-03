efa0d19da5496bc5b91bf8d548938b4f3173ba6b
Jesper Wistrom <theace4k@gmail.com>
2023-11-03 18:06:13 +0100

    Configure input arguments from command line

---
+++ src/args.rs
@@ -0,0 +1,43 @@
+use clap::{Args, Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Encode(EncodeArgs),
+    Decode(DecodeArgs),
+    Remove(RemoveArgs),
+    Print(PrintArgs),
+}
+
+#[derive(Args)]
+struct EncodeArgs {
+    file_path: PathBuf,
+    chunk_type: String,
+    message: String,
+    output_file: Option<PathBuf>,
+}
+
+#[derive(Args)]
+struct DecodeArgs {
+    file_path: PathBuf,
+    chunk_type: String,
+}
+
+#[derive(Args)]
+struct RemoveArgs {
+    file_path: PathBuf,
+    chunk_type: String,
+}
+
+#[derive(Args)]
+struct PrintArgs {
+    file_path: PathBuf,
+}
