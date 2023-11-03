57ff3c5d9021a54305696103c01312a5a0bcd378
Jesper Wistrom <theace4k@gmail.com>
2023-11-03 19:46:12 +0100

    Impl remove command

--- src/commands.rs
+++ src/commands.rs
@@ -1,17 +1,27 @@
 use anyhow::Result;
 use std::fs::File;
 use std::io::BufReader;
+use std::path::PathBuf;
 
 use crate::args::{DecodeArgs, EncodeArgs, PrintArgs, RemoveArgs};
 use crate::png::Png;
 
-pub fn encode(args: EncodeArgs);
-pub fn decode(args: DecodeArgs);
-pub fn remove(args: RemoveArgs);
-pub fn print(args: PrintArgs) -> Result<()> {
-    let file = File::open(args.file_path)?;
+fn read_file_from_file_path(file_path: PathBuf) -> Result<Png> {
+    let file = File::open(file_path)?;
     let buffer = BufReader::new(file);
     let png = Png::try_from(buffer.buffer())?;
+    Ok(png)
+}
+
+pub fn encode(args: EncodeArgs);
+pub fn decode(args: DecodeArgs);
+pub fn remove(args: RemoveArgs) -> Result<()> {
+    let mut png = read_file_from_file_path(args.file_path)?;
+    png.remove_chunk(&args.chunk_type)?;
+    Ok(())
+}
+pub fn print(args: PrintArgs) -> Result<()> {
+    let png = read_file_from_file_path(args.file_path)?;
     println!("{}", png);
     Ok(())
 }
