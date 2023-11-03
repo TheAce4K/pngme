c13b03cba32b6c4267886b56b42e0ec9738b24db
Jesper Wistrom <theace4k@gmail.com>
2023-11-03 19:26:38 +0100

    setup functions and parsing of commands

--- src/commands.rs
+++ src/commands.rs
@@ -1,1 +1,6 @@
-use crate::args::Cli;
+use crate::args::{DecodeArgs, EncodeArgs, PrintArgs, RemoveArgs};
+
+pub fn encode(args: EncodeArgs);
+pub fn decode(args: DecodeArgs);
+pub fn remove(args: RemoveArgs);
+pub fn print(args: PrintArgs);
--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,22 @@ use clap::Parser;
 fn main() -> Result<()> {
     let cli = args::Cli::parse();
     match cli.command {
-        args::Commands::Encode(args) => println!("encode {:?}", args),
-        args::Commands::Decode(args) => println!("decode {:?}", args),
-        args::Commands::Remove(args) => println!("remove {:?}", args),
-        args::Commands::Print(args) => println!("print {:?}", args),
+        args::Commands::Encode(args) => {
+            println!("encode {:#?}", args);
+            commands::encode(args);
+        }
+        args::Commands::Decode(args) => {
+            println!("decode {:?}", args);
+            commands::decode(args);
+        }
+        args::Commands::Remove(args) => {
+            println!("remove {:?}", args);
+            commands::remove(args);
+        }
+        args::Commands::Print(args) => {
+            println!("print {:?}", args);
+            commands::print(args);
+        }
     }
     Ok(())
 }
