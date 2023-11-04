b342a73b11a0b770e9178a249b713cf6db2506b3
Jesper Wistrom <theace4k@gmail.com>
2023-11-04 08:45:49 +0100

    Add doc comments to Args for more helpful command help

--- src/args.rs
+++ src/args.rs
@@ -10,35 +10,47 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    /// does testing things
+    /// Encodes a message into a chunk in the given file
     Encode(EncodeArgs),
+    /// Decodes a message from a given chunk type in a file
     Decode(DecodeArgs),
+    /// Removes a chunk from the given file
     Remove(RemoveArgs),
+    /// Prints the file given
     Print(PrintArgs),
 }
 
 #[derive(Args, Debug)]
 pub struct EncodeArgs {
+    /// Path to png file
     pub file_path: PathBuf,
+    /// Chunk type to be injected into file
     pub chunk_type: String,
+    /// Message encoded in chunk
     pub message: String,
+    /// Optional output file with this chunk injected
     pub output_file: Option<PathBuf>,
 }
 
 #[derive(Args, Debug)]
 pub struct DecodeArgs {
+    /// Path to png file
     pub file_path: PathBuf,
+    /// Chunk type to be injected into file
     pub chunk_type: String,
 }
 
 #[derive(Args, Debug)]
 pub struct RemoveArgs {
+    /// Path to png file
     pub file_path: PathBuf,
+    /// Chunk type to be injected into file
     pub chunk_type: String,
 }
 
 #[derive(Args, Debug)]
 pub struct PrintArgs {
+    /// Path to png file
     pub file_path: PathBuf,
 }
 
